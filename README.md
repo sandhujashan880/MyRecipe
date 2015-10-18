c63e6e981fbdd529a341f2c62e6113901b1c7189
jashansandhu880 <jashansnadhu880@gmail.com>
2015-10-18 21:22:36 +0000

    create chefs and recipes association and tests

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
+testing specs for recipes
+name: string
+description:text
+summary :text
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
-
-
-Welcome to your Rails project on Cloud9 IDE!
-
-To get started, just do the following:
-
-1. Run the project with the "Run Project" button in the menu bar on top of the IDE.
-2. Preview your new app by clicking on the URL that appears in the Run panel below (https://reciepemanager-jashansandhu880.c9.io/).
-
-Happy coding!
-The Cloud9 IDE team
+-name must be present
+-name must be between 5 to 100 characters
+summary must be present
+-summary must be between 20 to 150 characters
+-description must be present
+-description must be between 50 to 500 characters 
+-chef_id must be present
 
 
-## Support & Documentation
+testing spec for chefs
+chefname :string
+email :string
 
-Visit http://docs.c9.io for support, or to learn more about using Cloud9 IDE. 
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+-chefname must be present
+-chefname must be between 3 to 40 characters
+-email must be present
+-email miust be uniqe
+-email must be valid
+
---
+++ test/models/chef_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+  
+  def setup
+  @chef= Chef.new(chefname:"jashan", email:"jashansandhu880@gmail.com")
+  
+  end
+  
+    test "chef should be valid" do
+  
+    assert @chef.valid?
+    end
+  
+    test "chef name should be present" do
+  
+    @chef.chefname= " "
+    assert_not @chef.valid?
+    end
+    
+    test "chef name should not be too long" do
+      @chef.chefname= "a" * 41
+      assert_not @chef.valid?
+    end
+    
+    test "chefname should not be too short" do
+      @chef.chefname= "a" * 2
+      assert_not @chef.valid?
+    end
+    
+    test "email should be present" do
+      @chef.email=""
+      assert_not @chef.valid?
+    end
+    
+    test "emal should be in bounds" do
+      @chef.email= "a" * 101 + "@example.com"
+      assert_not @chef.valid?
+    end
+  
+  test "email address should unique" do
+    dup_chef = @chef.dup
+    dup_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not dup_chef.valid?
+  end
+  
+  test "email should accept be valid address" do
+   valid_emails= %w[abc@eee.com ABC_dd-w@eee.com aaa@gmail.com abc123@gmail.com]
+   valid_emails.each do |va|
+     @chef.email = va 
+     assert @chef.valid?, '#{va.inspect} is a valid email address'
+   end
+  end
+  
+  
+  test "email should reject be invalid address" do
+   invalid_emails= %w[abc.com abc123.org.com abc@com abc 123 @112. org ]
+   invalid_emails.each do |ia|
+     @chef.email = ia
+     assert_not @chef.valid?, '#{ia.inspect} is a invalid address'
+   end
+  end
+  
+end
