2dba9ca5dda19fc474c2c54dc8656f0c3ad1cbc9
jashansandhu880 <jashansnadhu880@gmail.com>
2016-01-21 19:35:09 +0000

    heroku final push

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,7 +10,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  if Rails.env.production?
+    storage :fog
+  else
+    
   storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
