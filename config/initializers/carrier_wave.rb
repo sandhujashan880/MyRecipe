9f1da7065102a524f42b14d3b47de84644c10c36
jashansandhu880 <jashansnadhu880@gmail.com>
2016-01-21 21:08:14 +0000

    new

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
-    :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
+    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
    
   }
   config.fog_directory  = ENV['S3_BUCKET']                             # required
