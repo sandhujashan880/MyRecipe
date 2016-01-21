e2051517a43cd3ff188bfc5481da6002b0007bae
jashansandhu880 <jashansnadhu880@gmail.com>
2016-01-21 21:05:00 +0000

    new

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,11 +6,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
     :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+   
   }
-  config.fog_directory  = ENV['S3_Bucket']                             # required
+  config.fog_directory  = ENV['S3_BUCKET']                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
